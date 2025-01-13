d6791252413c4bee8a72504ca91dbaa7aa103e33
Sushant <sushantsxzz5@gmail.com>
2025-01-14 00:48:28 +0530

    Added Landing Page to the website.
    - Now the root path points to the hero component. And setting the finalised streamId is done from that page itself.
    - The App.jsx component now exists for my personal testing to quickly go to the stream and watch pages. Coming from the landing page, the streamId are finalised already and we simply jump to the videofeed screens.
    - I have not implemented backend checking for the validity of streamID yet, I am too tired.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,13 @@ import { Routes } from 'react-router'
 import { Route } from 'react-router'
 import Pub from './pub.jsx'
 import Sub from './sub.jsx'
+import Hero from './Hero.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
-        <Route path="/" element={<App/>}></Route>
+        <Route path="/" element={<Hero/>}></Route>
+        <Route path="/app" element={<App/>}></Route>
         <Route path="/stream" element={<Pub/>}></Route>
         <Route path="/watch" element={<Sub/>}></Route>
         </Routes>
